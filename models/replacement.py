ad87b6845ad3a60ed5ffae4647743d8a768b53d3
AlfaSystemas5457 <software@alfasystemas.com.mx>
2025-09-12 17:03:16 -0600

    changue description of Text to Html and add access csv to equipment_return_wizard

--- models/replacement.py
+++ models/replacement.py
@@ -36,7 +36,7 @@ class Replacement(models.Model):
 
     area = fields.Many2one('equipment.area', string='Area',
                            track_visibility='onchange', track_sequence=2, required=True)
-    description = fields.Text(string='Descripción')
+    description = fields.Html(string='Descripción')
     state = fields.Selection(
         [
             ('draft', 'Nuevo'),
