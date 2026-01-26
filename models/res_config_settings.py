d269a1dce16dede8d00f55f95d6956721c7303d3
Diego <Diego@DESKTOP-C0319P1>
2026-01-26 14:08:03 -0600

    fix: refactor equipment settings and user access scope handling for improved clarity and functionality

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -4,44 +4,61 @@ from odoo import models, fields, api
 
 
 class EquipmentSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     enable_return_emails = fields.Boolean(
-        'Enviar correos de recordatorio de devolución',
-        config_parameter='equipment_allocation.enable_return_emails'
+        "Enviar correos de recordatorio de devolución",
+        config_parameter="equipment_allocation.enable_return_emails",
     )
 
 
 class UsersSettings(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     access_scope_equipment = fields.Boolean(
-        'Acceso a todas las opciones de asignaciones de equipos', default=False,
-        help='Permite al usuario ver y gestionar todas las asignaciones de equipos sin restricciones.')
+        "Acceso a todas las opciones de asignaciones de equipos",
+        default=False,
+        help="Permite al usuario ver y gestionar todas las asignaciones de equipos sin restricciones.",
+    )
 
     def _update_groups_based_on_scope(self, access_scope=False):
         group_equipment_replacement = self.env.ref(
-            'equipment_allocation.group_equipment_replacement_access', raise_if_not_found=False)
+            "equipment_allocation.group_equipment_replacement_access",
+            raise_if_not_found=False,
+        )
         group_equipment_allocations = self.env.ref(
-            'equipment_allocation.group_equipment_allocations_access', raise_if_not_found=False)
+            "equipment_allocation.group_equipment_allocations_access",
+            raise_if_not_found=False,
+        )
 
         for user in self:
             if not group_equipment_replacement or not group_equipment_allocations:
                 continue
 
             if access_scope:
-                user.groups_id = user.groups_id | group_equipment_allocations | group_equipment_replacement
+                user.groups_id = (
+                    user.groups_id
+                    | group_equipment_allocations
+                    | group_equipment_replacement
+                )
             else:
-                user.groups_id = user.groups_id - \
-                    group_equipment_replacement - group_equipment_allocations
+                user.groups_id = (
+                    user.groups_id
+                    - group_equipment_replacement
+                    - group_equipment_allocations
+                )
 
     def write(self, vals):
-        if 'access_scope_equipment' in vals:
-            self._update_groups_based_on_scope(vals['access_scope_equipment'])
+        if "access_scope_equipment" in vals:
+            self._update_groups_based_on_scope(vals["access_scope_equipment"])
         return super().write(vals)
 
+    @api.model_create_multi
     def create(self, vals_list):
-        if 'access_scope_equipment' in vals_list:
-            self._update_groups_based_on_scope(
-                vals_list['access_scope_equipment'])
-        return super().create(vals_list)
+        records = super().create(vals_list)
+        for record, vals in zip(records, vals_list):
+            if vals.get("access_scope_equipment"):
+                record._update_groups_based_on_scope(True)
+            else:
+                record._update_groups_based_on_scope(False)
+        return records
